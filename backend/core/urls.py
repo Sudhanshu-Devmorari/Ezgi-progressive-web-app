d141b366d5a6b84dbb2d6f7d164ec7ae40ebafb5
nehakhedkar4 <nehakhedkar30@gmail.com>
2023-10-04 17:47:59 +0530

    fix the view file and remove commented code

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -6,7 +6,7 @@ from core.views import (RetrieveCommentatorView, FollowCommentatorView, CommentV
                          GetPendingBalance,
         
                          RetrieveSubscriberListAndSubscriptionList, DeactivateProfile, SignupView, OtpVerify, OtpReSend, LoginView, PasswordResetView, RetrieveSubscriberListAndSubscriptionList, DeactivateProfile, OtpSend,
-                         GoogleLoginview, FacebookLoginview, VerifyUserView, SignupUserExistsView, FootbalAndBasketballContentView, CheckDeactivatedAccount, GetUserdata, HighlightPurchaseView, EditorBannerView, GetFutbolAndBasketbolCountView, TestCronView)
+                         GoogleLoginview, FacebookLoginview, VerifyUserView, SignupUserExistsView, FootbalAndBasketballContentView, CheckDeactivatedAccount, GetUserdata, HighlightPurchaseView, EditorBannerView, GetFutbolAndBasketbolCountView)
 
 
 from core.views import (AdminMainPage, UserManagement, FilterUserManagement, CommentsManagement, FilterComments, EditorManagement, EditorSubscriptionDetails,
@@ -108,5 +108,4 @@ urlpatterns = [
     path('highlight-purchase/', HighlightPurchaseView.as_view(), name='User-Data'),
     path('editor-banner/', EditorBannerView.as_view(), name='Editor-banner'),
     path('futbol-basketbol-count/', GetFutbolAndBasketbolCountView.as_view(), name='futbol-basketbol-count'),
-    path('test-cron/', TestCronView.as_view(), name='test-cron'),
 ]
